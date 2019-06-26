362b033f6ed706a5482f8de664c449ce80ab4a1e
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2019-06-26 13:51:28 +0530

    basic c++ tricks

---
+++ Tricks_for_CP.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int32_t main()
+{
+	auto a="AX Sfsag";
+	cout<<a<<endl;
+	int x;
+	cout<<"size of x "<<sizeof(x)<<endl;
+	//use auto keyword when you didnot know the data type of variable
+	//use in the auto keyword in the iterator(when we defined an iterator
+	cout<<sizeof(a);
+}
